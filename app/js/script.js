3685902d4c0ff0e150a5a6b3a391c43bc115f031
Sean Cesmat <sean.cesmat@gmail.com>
2015-10-16 21:31:39 -0700

    Ran Grunt, Clean Up

--- app/js/script.js
+++ app/js/script.js
@@ -1,91 +1,97 @@
+'use strict';
+
 $(function() {
-    
-   $('#quote').click(function() {
-       //$('#adjective').text('Hello Ya\'ll');
-        var checkedAdj = document.getElementById("includeAdj").checked;
-        var checkedNoun = document.getElementById("includeNoun").checked;
-        var checkedVerb = document.getElementById("includeVerb").checked;
-        
-        if (checkedAdj === true) {
-            $.get("/adjective", function(response) {
-                var adjective = response.word;
-                $("#adjective").text(adjective + "  ");
-            });
-        }else if (checkedAdj === false) {
-            $('#adjective').empty();
-        }
-        
-        if (checkedVerb === true) {
-            $.get("/verb", function(response) {
-               var verb = response.word;
-               $("#verb").text(verb); 
-            });
-        }else if (checkedVerb === false) {
-            $('#verb').empty();
-        }
-        
-        if (checkedNoun === true) {
-            $.get("/noun", function(response) {
-               var noun = response.word;
-               $("#noun").text(noun + "  "); 
-            });
-         }else if (checkedNoun === false) {
-            $('#noun').empty();
-        }
-   });
-   
-   $('#submitWords').on('submit', function(e) {
-      e.preventDefault();
-      
-    
-      $(':input').removeAttr('placeholder');
-      var adjective = $('input[name=adjective]').val();
-      var verb = $('input[name=verb]').val();
-      var noun = $('input[name=noun]').val();
-      var adjPost;
-      var verbPost;
-      var nounPost;
-      
-       $("#adjectiveRes").stop(true, true).hide().fadeIn(2000).delay(3000).fadeOut(2000);
-       $("#nounRes").stop(true, true).hide().fadeIn(2000).delay(3000).fadeOut(2000);
-       $("#verbRes").stop(true, true).hide().fadeIn(2000).delay(3000).fadeOut(2000);
-      
-      if (adjective) {
-          adjPost = {word: adjective};
-          $.post('adjective', adjPost, function(response) {
-             var adjectiveRes = response.msg;
-             $('#adjectiveRes').text(adjectiveRes); 
-          });
-         
-      }
-      
-      if (verb) {
-          verbPost = {word: verb};
-          $.post('verb', verbPost, function(response) {
-             var verbRes = response.msg;
-             $('#verbRes').text(verbRes); 
-          });
-          $('input[name=verb]').attr('placeholder', "Verb");
-      }
-      
-      if (noun) {
-          nounPost = {word: noun};
-          $.post('noun', nounPost, function(response) {
-             var nounRes = response.msg;
-             $('#nounRes').text(nounRes); 
-          });
-          
-      }
-         $('#adjectiveRes').empty();
-      $('#verbRes').empty();
-      $('#nounRes').empty();
-       
-       $('input[name=adjective]').attr('placeholder', "adjective");
-       $('input[name=verb]').attr('placeholder', "verb");
-       $('input[name=noun]').attr('placeholder', "noun");
-       $('input[type=text]').val('');
-   });
-   
-   
-   //console.log(getRandomWord(adjective)); 
+
+  $('#quote').click(function() {
+    //$('#adjective').text('Hello Ya\'ll');
+    var checkedAdj = $('#includeAdj').checked;
+    var checkedNoun = $('#includeNoun').checked;
+    var checkedVerb = $('#includeVerb').checked;
+
+    if (checkedAdj === true) {
+      $.get('/adjective', function(response) {
+        var adjective = response.word;
+        $('#adjective').text(adjective + '  ');
+      });
+    } else if (checkedAdj === false) {
+      $('#adjective').empty();
+    }
+
+    if (checkedVerb === true) {
+      $.get('/verb', function(response) {
+        var verb = response.word;
+        $('#verb').text(verb);
+      });
+    } else if (checkedVerb === false) {
+      $('#verb').empty();
+    }
+
+    if (checkedNoun === true) {
+      $.get('/noun', function(response) {
+        var noun = response.word;
+        $('#noun').text(noun + '  ');
+      });
+    } else if (checkedNoun === false) {
+      $('#noun').empty();
+    }
+  });
+
+  $('#submitWords').on('submit', function(e) {
+    e.preventDefault();
+
+    $(':input').removeAttr('placeholder');
+    var adjective = $('input[name=adjective]').val();
+    var verb = $('input[name=verb]').val();
+    var noun = $('input[name=noun]').val();
+    var adjPost;
+    var verbPost;
+    var nounPost;
+
+    $('#adjectiveRes').stop(true, true).hide().fadeIn(2000).delay(3000).fadeOut(2000);
+    $('#nounRes').stop(true, true).hide().fadeIn(2000).delay(3000).fadeOut(2000);
+    $('#verbRes').stop(true, true).hide().fadeIn(2000).delay(3000).fadeOut(2000);
+
+    if (adjective) {
+      adjPost = {
+        word: adjective
+      };
+      $.post('adjective', adjPost, function(response) {
+        var adjectiveRes = response.msg;
+        $('#adjectiveRes').text(adjectiveRes);
+      });
+    }
+
+    if (verb) {
+      verbPost = {
+        word: verb
+      };
+      $.post('verb', verbPost, function(response) {
+        var verbRes = response.msg;
+        $('#verbRes').text(verbRes);
+      });
+
+      $('input[name=verb]').attr('placeholder', 'Verb');
+    }
+
+    if (noun) {
+      nounPost = {
+        word: noun
+      };
+      $.post('noun', nounPost, function(response) {
+        var nounRes = response.msg;
+        $('#nounRes').text(nounRes);
+      });
+    }
+
+    $('#adjectiveRes').empty();
+    $('#verbRes').empty();
+    $('#nounRes').empty();
+
+    $('input[name=adjective]').attr('placeholder', 'adjective');
+    $('input[name=verb]').attr('placeholder', 'verb');
+    $('input[name=noun]').attr('placeholder', 'noun');
+    $('input[type=text]').val('');
+  });
+
+  //console.log(getRandomWord(adjective));
 });
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var express = require("express");
+var express = require('express');
 var app = express();
-var bodyparser = require("body-parser");
+var bodyparser = require('body-parser');
 var port = process.env.PORT || 3000;
-var Adjectives = require("./lib/adjectives.js");
-var Nouns = require("./lib/nouns.js");
-var Verbs = require("./lib/verbs.js");
-var getRandomWord = require("./lib/getRandomWord.js");
-var postWord = require("./lib/postWord.js");
+var Adjectives = require('./lib/adjectives.js');
+var Nouns = require('./lib/nouns.js');
+var Verbs = require('./lib/verbs.js');
+var getRandomWord = require('./lib/getRandomWord.js');
+var postWord = require('./lib/postWord.js');
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
@@ -18,12 +18,13 @@ app.use(bodyparser.urlencoded({
 app.use(express.static(__dirname + '/app/'));
 
 app.listen(port, function() {
-   console.log('server started on port ' + port); 
+  console.log('server started on port ' + port);
 });
+
 console.log(port);
 /*
-app.get("/", function(req, res) {
-   res.send("Hello Universe!"); 
+app.get('/', function(req, res) {
+   res.send('Hello Universe!');
 });
 */
 
@@ -31,34 +32,33 @@ var adjective = new Adjectives();
 var verb = new Verbs();
 var noun = new Nouns();
 
-
-app.get("/adjective", function(req, res) {
-   res.json(getRandomWord(adjective)); 
+app.get('/adjective', function(req, res) {
+  res.json(getRandomWord(adjective));
 });
 
-app.get("/noun", function(req, res) {
-   res.json(getRandomWord(noun)); 
+app.get('/noun', function(req, res) {
+  res.json(getRandomWord(noun));
 });
 
-app.get("/verb", function(req, res) {
-    res.json(getRandomWord(verb));
+app.get('/verb', function(req, res) {
+  res.json(getRandomWord(verb));
 });
 
-app.get("/", function(req, res) {
-   res.sendFile(index.html); 
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
 });
 
-app.post("/adjective", function(req, res) {
-   var word = postWord(req.body.word, adjective);
-   res.json(word); 
+app.post('/adjective', function(req, res) {
+  var word = postWord(req.body.word, adjective);
+  res.json(word);
 });
 
-app.post("/verb", function(req, res) {
-   var word = postWord(req.body.word, verb);
-   res.json(word); 
+app.post('/verb', function(req, res) {
+  var word = postWord(req.body.word, verb);
+  res.json(word);
 });
 
 app.post('/noun', function(req, res) {
-   var word = postWord(req.body.word, noun);
-   res.json(word); 
+  var word = postWord(req.body.word, noun);
+  res.json(word);
 });
